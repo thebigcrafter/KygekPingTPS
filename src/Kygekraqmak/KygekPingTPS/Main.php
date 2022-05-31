78ae4fdd0fd59717794100ec0034bbf73267e44e
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-05-31 12:26:11 +0700

    `warnDevelopmentVersion()` instead of a direct warning

--- src/Kygekraqmak/KygekPingTPS/Main.php
+++ src/Kygekraqmak/KygekPingTPS/Main.php
@@ -41,12 +41,13 @@ class Main extends PluginBase {
 
 	public function onEnable(): void {
 		self::$instance = $this;
+		$ktpmplCfs = new KtpmplCfs($this);
 		$this->saveDefaultConfig();
 		if ($this->isDev) {
-			$this->getLogger()->warning("This plugin is running on a development version. There might be some major bugs. If you found one, please submit an issue in https://github.com/thebigcrafter/KygekPingTPS/issues.");
+			$ktpmplCfs->warnDevelopmentVersion();
 		}
-		KtpmplCfs::checkUpdates($this);
-		KtpmplCfs::checkConfig($this, "2.0");
+		$ktpmplCfs->checkUpdates();
+		$ktpmplCfs->checkConfig("2.0");
 	}
 
 	public static function getInstance(): self {
